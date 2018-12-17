85b6f419cc67efe9b76e6d18e8816f0a137af953
zloyleva <zloyleva@gmail.com>
2018-12-17 12:54:17 +0200

    add tests

--- test_webapp.py
+++ test_webapp.py
@@ -1,9 +1,14 @@
 import unittest
+import requests
 
 class TestApiMethods(unittest.TestCase):
 
-    def test_pin_off_all(self):
+	def test_pin_off_all(self):
 		r = requests.get('http://192.168.1.6/pin/off/all')
+		self.assertEqual(r.status_code, 200)
+
+	def test_pin_on_all(self):
+		r = requests.get('http://192.168.1.6/pin/on/all')
 		self.assertEqual(r.status_code, 200)
 
 if __name__ == '__main__':
