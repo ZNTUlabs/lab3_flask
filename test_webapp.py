6771ba19090dbf2cf8632293bbfeca9b2e3639aa
zloyleva <zloyleva@gmail.com>
2018-12-17 14:24:09 +0200

    modify test

--- test_webapp.py
+++ test_webapp.py
@@ -2,13 +2,15 @@ import unittest
 import requests
 
 class TestApiMethods(unittest.TestCase):
+	def getServerUrl(self):
+		return "http://192.168.43.159"
 
 	def test_pin_off_all(self):
-		r = requests.get('http://192.168.1.6/pin/off/all')
+		r = requests.get(self.getServerUrl() + '/pin/off/all')
 		self.assertEqual(r.status_code, 200)
 
 	def test_pin_on_all(self):
-		r = requests.get('http://192.168.1.6/pin/on/all')
+		r = requests.get(self.getServerUrl() + '/pin/on/all')
 		self.assertEqual(r.status_code, 200)
 
 if __name__ == '__main__':
